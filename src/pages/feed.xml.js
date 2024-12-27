4aa1568bddf5245fd748c599d71ee70d1723fc32
Chaleb Pommells <chaleb@hey.com>
2024-12-27 01:32:59 -0700

    🔧 (src/pages/feed.xml.js): Update import statements and fix minor syntax errors for better compatibility with AstroJS RSS feed generation

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,20 +1,20 @@
-mport rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-	const blog = await getCollection("blog");
-	return rss({
-		title: "Chaleb Dev Blog",
-		description: "Personal, programming, and essays from Chaleb",
-		stylesheet: false,
-		site: context.site,
-		items: blog.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			link: `/blog/${post.slug}/`,
-		})),
-		customData: "<language>en-us</language>",
-		canonicalUrl: "https://www.iamcpdev.me",
-	});
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Chaleb Dev Blog',
+    description: 'Personal, programming, and essays from Chaleb',
+    stylesheet: false,
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: '<language>en-us</language>',
+    canonicalUrl: 'https://www.iamcpdev.me',
+  });
 }
